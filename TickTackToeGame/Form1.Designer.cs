e5771860be595d3251ba5c2ff7301899bf024758
Home <Home@GAMES-PC>
2023-09-15 21:57:20 +0100

    Add project files.

---
+++ TickTackToeGame/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace TickTackToeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SelectDifficulty = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tableLayoutPanel1.AutoScroll = true;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.button9, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button8, 1, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(92, 162);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(702, 655);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(469, 439);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(230, 213);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(236, 439);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(227, 213);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(3, 439);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(227, 213);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(469, 221);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(230, 212);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(236, 221);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(227, 212);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(3, 221);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(227, 212);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(469, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(230, 212);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(236, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(227, 212);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(227, 212);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SelectDifficulty
+            // 
+            this.SelectDifficulty.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.SelectDifficulty.Location = new System.Drawing.Point(328, 77);
+            this.SelectDifficulty.Name = "SelectDifficulty";
+            this.SelectDifficulty.Size = new System.Drawing.Size(227, 47);
+            this.SelectDifficulty.TabIndex = 1;
+            this.SelectDifficulty.Text = "Change Difficulty to Easy?";
+            this.SelectDifficulty.UseVisualStyleBackColor = true;
+            this.SelectDifficulty.Click += new System.EventHandler(this.SelectDifficulty_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(153, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(580, 55);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Welcome To TickTackToe";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 860);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SelectDifficulty);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button SelectDifficulty;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TickTackToeGame/Form1.cs
@@ -0,0 +1,365 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TickTackToeGame
+{
+    public partial class Form1 : Form
+    {
+        private char currentPlayer = 'X'; // 'X' starts the game
+        private bool gameEnded = false;
+        private Button[,] buttons;
+        static string Difficulty = "Hard";
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            buttons = new Button[3, 3] {
+                { button1, button2, button3 },
+                { button4, button5, button6 },
+                { button7, button8, button9 }
+            };
+
+            foreach (Button button in buttons)
+            {
+                button.Text = "";
+                button.Enabled = true;
+
+                // Remove the event handler if it's already attached
+                button.Click -= Button_Click;
+
+                // Add the event handler back
+                button.Click += Button_Click;
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (button.Text == "" && !gameEnded)
+            {
+                button.Text = currentPlayer.ToString();
+                button.Enabled = false;
+                CheckForWinner();
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+
+                if (currentPlayer == 'O' && !gameEnded)
+                {
+                    // Computer's turn to make a random move
+                    ComputerMove();
+                }
+            }
+        }
+
+        private void ComputerMove()
+        {
+            List<Button> availableButtons = new List<Button>();
+            char opponent = (currentPlayer == 'X') ? 'O' : 'X'; // Determine the opponent's symbol
+
+            // Check if the computer can win on the next move
+            foreach (Button button in buttons)
+            {
+                if (button.Text == "")
+                {
+                    button.Text = "O";
+                    if (CheckForWin("O") && Difficulty == "Hard")
+                    {
+                        button.Enabled = false;
+                        currentPlayer = 'X'; // Switch back to the player
+                        return;
+                    }
+                    button.Text = ""; // Reset the button
+                }
+            }
+
+            // Check if the player can win on the next move and block them
+            foreach (Button button in buttons)
+            {
+                if (button.Text == "")
+                {
+                    button.Text = "X";
+                    if (CheckForWin("X") && Difficulty == "Hard")
+                    {
+                        button.Text = "O";
+                        button.Enabled = false;
+                        currentPlayer = 'X'; // Switch back to the player
+                        return;
+                    }
+                    button.Text = ""; // Reset the button
+                }
+            }
+
+            // Check if the player is about to win and block them
+            foreach (Button button in buttons)
+            {
+                if (button.Text == "")
+                {
+                    button.Text = "X"; // Simulate player's move
+                    if (IsPlayerAboutToWin('X') && Difficulty == "Hard")
+                    {
+                        button.Text = "O";
+                        button.Enabled = false;
+                        currentPlayer = 'X'; // Switch back to the player
+                        return;
+                    }
+                    button.Text = ""; // Reset the button
+                }
+            }
+
+            // If no winning or blocking moves, make a random move
+            foreach (Button button in buttons)
+            {
+                if (button.Text == "")
+                {
+                    availableButtons.Add(button);
+                }
+            }
+
+            if (availableButtons.Count > 0)
+            {
+                Random random = new Random();
+                int index = random.Next(availableButtons.Count);
+                Button computerChoice = availableButtons[index];
+                computerChoice.Text = "O";
+                computerChoice.Enabled = false;
+                currentPlayer = 'X'; // Switch back to the player
+            }
+        }
+
+        private void CheckForWinner()
+        {
+            // Check for a win condition here
+            // You need to implement the logic for checking rows, columns, and diagonals.
+            // If a win condition is met, set gameEnded to true and show a message box.
+            for (int row = 0; row < 3; row++)
+            {
+                if (buttons[row, 0].Text != "" && buttons[row, 0].Text == buttons[row, 1].Text && buttons[row, 1].Text == buttons[row, 2].Text)
+                {
+                    EndGame(buttons[row, 0].Text);
+                    return;
+                }
+            }
+
+            // Check columns
+            for (int col = 0; col < 3; col++)
+            {
+                if (buttons[0, col].Text != "" && buttons[0, col].Text == buttons[1, col].Text && buttons[1, col].Text == buttons[2, col].Text)
+                {
+                    EndGame(buttons[0, col].Text);
+                    return;
+                }
+            }
+
+            // Check diagonals
+            if (buttons[0, 0].Text != "" && buttons[0, 0].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[2, 2].Text)
+            {
+                EndGame(buttons[0, 0].Text);
+                return;
+            }
+
+            if (buttons[0, 2].Text != "" && buttons[0, 2].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[2, 0].Text)
+            {
+                EndGame(buttons[0, 2].Text);
+                return;
+            }
+
+            // Check for a tie (all buttons filled)
+            bool isTie = true;
+            foreach (Button button in buttons)
+            {
+                if (button.Text == "")
+                {
+                    isTie = false;
+                    break;
+                }
+            }
+
+            if (isTie && !gameEnded) // Add !gameEnded to ensure the game hasn't already ended in a win
+            {
+                EndGame("Tie");
+            }
+        }
+        private void EndGame(string winner)
+        {
+            gameEnded = true;
+
+            if (winner == "Tie")
+            {
+                MessageBox.Show("It's a tie!", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show(winner + " wins!", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+             // Call the ResetGame method to start a new game
+        }
+
+        private bool IsPlayerAboutToWin(char playerSymbol)
+        {
+            // Check rows
+            for (int row = 0; row < 3; row++)
+            {
+                int count = 0; // Count of player's symbols in the row
+                int emptyCount = 0; // Count of empty cells in the row
+                for (int col = 0; col < 3; col++)
+                {
+                    if (buttons[row, col].Text == playerSymbol.ToString())
+                    {
+                        count++;
+                    }
+                    else if (buttons[row, col].Text == "")
+                    {
+                        emptyCount++;
+                    }
+                }
+
+                if (count == 2 && emptyCount == 1)
+                {
+                    return true;
+                }
+            }
+
+            // Check columns
+            for (int col = 0; col < 3; col++)
+            {
+                int count = 0; // Count of player's symbols in the column
+                int emptyCount = 0; // Count of empty cells in the column
+                for (int row = 0; row < 3; row++)
+                {
+                    if (buttons[row, col].Text == playerSymbol.ToString())
+                    {
+                        count++;
+                    }
+                    else if (buttons[row, col].Text == "")
+                    {
+                        emptyCount++;
+                    }
+                }
+
+                if (count == 2 && emptyCount == 1)
+                {
+                    return true;
+                }
+            }
+
+            // Check diagonals
+            int diagonal1Count = 0;
+            int diagonal2Count = 0;
+            int diagonal1EmptyCount = 0;
+            int diagonal2EmptyCount = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (buttons[i, i].Text == playerSymbol.ToString())
+                {
+                    diagonal1Count++;
+                }
+                else if (buttons[i, i].Text == "")
+                {
+                    diagonal1EmptyCount++;
+                }
+
+                if (buttons[i, 2 - i].Text == playerSymbol.ToString())
+                {
+                    diagonal2Count++;
+                }
+                else if (buttons[i, 2 - i].Text == "")
+                {
+                    diagonal2EmptyCount++;
+                }
+            }
+
+            if (diagonal1Count == 2 && diagonal1EmptyCount == 1)
+            {
+                return true;
+            }
+
+            if (diagonal2Count == 2 && diagonal2EmptyCount == 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        private bool CheckForWin(string playerSymbol)
+        {
+            // Check rows
+            for (int row = 0; row < 3; row++)
+            {
+                if (buttons[row, 0].Text == playerSymbol && buttons[row, 1].Text == playerSymbol && buttons[row, 2].Text == playerSymbol)
+                {
+                    return true;
+                }
+            }
+
+            // Check columns
+            for (int col = 0; col < 3; col++)
+            {
+                if (buttons[0, col].Text == playerSymbol && buttons[1, col].Text == playerSymbol && buttons[2, col].Text == playerSymbol)
+                {
+                    return true;
+                }
+            }
+
+            // Check diagonals
+            if (buttons[0, 0].Text == playerSymbol && buttons[1, 1].Text == playerSymbol && buttons[2, 2].Text == playerSymbol)
+            {
+                return true;
+            }
+
+            if (buttons[0, 2].Text == playerSymbol && buttons[1, 1].Text == playerSymbol && buttons[2, 0].Text == playerSymbol)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SelectDifficulty_Click(object sender, EventArgs e)
+        {
+            if (Difficulty == "Hard")
+            {
+                Difficulty = "Easy";
+                SelectDifficulty.Text = "Change Difficulty To Hard?";
+            }
+            else
+            {
+                Difficulty = "Hard";
+                SelectDifficulty.Text = "Change Difficulty To Easy?";
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
