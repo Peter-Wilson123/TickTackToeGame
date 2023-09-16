4be973bdebbee2ebd3aa885069284e878c7b879e
Peter Wilson <pwilson455@icloud.com>
2023-09-16 15:46:11 +0100

    AddedReset

--- TickTackToeGame/Form1.Designer.cs
+++ TickTackToeGame/Form1.Designer.cs
@@ -31,7 +31,6 @@ namespace TickTackToeGame
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.button9 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
@@ -39,8 +38,10 @@ namespace TickTackToeGame
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             this.SelectDifficulty = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.ResetButton = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -74,22 +75,12 @@ namespace TickTackToeGame
             // button9
             // 
             this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button9.Location = new System.Drawing.Point(469, 439);
+            this.button9.Location = new System.Drawing.Point(471, 439);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(230, 213);
+            this.button9.Size = new System.Drawing.Size(228, 213);
             this.button9.TabIndex = 8;
             this.button9.Text = "button9";
             this.button9.UseVisualStyleBackColor = true;
-            // 
-            // button8
-            // 
-            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button8.Location = new System.Drawing.Point(236, 439);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(227, 213);
-            this.button8.TabIndex = 7;
-            this.button8.Text = "button8";
-            this.button8.UseVisualStyleBackColor = true;
             // 
             // button7
             // 
@@ -104,9 +95,9 @@ namespace TickTackToeGame
             // button6
             // 
             this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(469, 221);
+            this.button6.Location = new System.Drawing.Point(471, 221);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(230, 212);
+            this.button6.Size = new System.Drawing.Size(228, 212);
             this.button6.TabIndex = 5;
             this.button6.Text = "button6";
             this.button6.UseVisualStyleBackColor = true;
@@ -114,7 +105,7 @@ namespace TickTackToeGame
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(236, 221);
+            this.button5.Location = new System.Drawing.Point(237, 221);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(227, 212);
             this.button5.TabIndex = 4;
@@ -134,9 +125,9 @@ namespace TickTackToeGame
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(469, 3);
+            this.button3.Location = new System.Drawing.Point(471, 3);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(230, 212);
+            this.button3.Size = new System.Drawing.Size(228, 212);
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
@@ -144,7 +135,7 @@ namespace TickTackToeGame
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(236, 3);
+            this.button2.Location = new System.Drawing.Point(237, 3);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(227, 212);
             this.button2.TabIndex = 1;
@@ -162,12 +153,22 @@ namespace TickTackToeGame
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(237, 439);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(227, 213);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
             // SelectDifficulty
             // 
             this.SelectDifficulty.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.SelectDifficulty.Location = new System.Drawing.Point(328, 77);
+            this.SelectDifficulty.Location = new System.Drawing.Point(282, 77);
             this.SelectDifficulty.Name = "SelectDifficulty";
-            this.SelectDifficulty.Size = new System.Drawing.Size(227, 47);
+            this.SelectDifficulty.Size = new System.Drawing.Size(321, 79);
             this.SelectDifficulty.TabIndex = 1;
             this.SelectDifficulty.Text = "Change Difficulty to Easy?";
             this.SelectDifficulty.UseVisualStyleBackColor = true;
@@ -185,11 +186,23 @@ namespace TickTackToeGame
             this.label1.Text = "Welcome To TickTackToe";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // ResetButton
+            // 
+            this.ResetButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ResetButton.Location = new System.Drawing.Point(365, 820);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(153, 43);
+            this.ResetButton.TabIndex = 3;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(995, 860);
+            this.Controls.Add(this.ResetButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.SelectDifficulty);
             this.Controls.Add(this.tableLayoutPanel1);
@@ -216,6 +229,7 @@ namespace TickTackToeGame
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button SelectDifficulty;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ResetButton;
     }
 }
 
